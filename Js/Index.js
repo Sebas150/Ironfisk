85ed6001fd6ce089450bf5cc553653c6512dc9ae
Sebas150 <106000497+Sebas150@users.noreply.github.com>
2022-11-14 01:30:52 -0500

    v6

---
+++ Js/Index.js
@@ -0,0 +1,8 @@
+Swal.fire({
+            title: '¿Preparado para salvar el mundo? <br><br> <img src="IMG/planeta_tierra.png" width = "120px"><br>',
+            html: 'IRON FIST, es un juego que mejorara tus reflejos a medida que pases de nivel, retandote cada vez mas a medida que avances y desbloqueando grandes logros al final de cada nivel, esperamos te diviertas y disfrutes de este gran juego   ',
+            icon: 'sucess',
+            confirmButtonText: 'ESTOY PREPARADO',
+            width: '50%',
+            height: '80%',
+            timer: 100000,
