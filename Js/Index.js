3742514d83e2ef451fb682c72b9ba6a58835856b
Sebas150 <106000497+Sebas150@users.noreply.github.com>
2022-11-14 01:32:23 -0500

    v6

--- Js/Index.js
+++ Js/Index.js
@@ -1,8 +1,16 @@
 Swal.fire({
-            title: '¿Preparado para salvar el mundo? <br><br> <img src="IMG/planeta_tierra.png" width = "120px"><br>',
-            html: 'IRON FIST, es un juego que mejorara tus reflejos a medida que pases de nivel, retandote cada vez mas a medida que avances y desbloqueando grandes logros al final de cada nivel, esperamos te diviertas y disfrutes de este gran juego   ',
-            icon: 'sucess',
-            confirmButtonText: 'ESTOY PREPARADO',
-            width: '50%',
-            height: '80%',
-            timer: 100000,
+    title: '¿Preparado para salvar el mundo? <br><br> <img src="IMG/planeta_tierra.png" width = "120px"><br>',
+    html: 'IRON FIST, es un juego que mejorara tus reflejos a medida que pases de nivel, retandote cada vez mas a medida que avances y desbloqueando grandes logros al final de cada nivel, esperamos te diviertas y disfrutes de este gran juego   ',
+    icon: 'sucess',
+    confirmButtonText: 'ESTOY PREPARADO',
+    width: '50%',
+    height: '80%',
+    timer: 100000,
+
+    timerProgressbar: true,
+    /*Funcion de cerrar la alerta*/
+    allowOutsideClick: true,
+    allowEscapeKey: false,
+    allowEnterkey: false,
+    stopKeydownPropagation: false,
+});
